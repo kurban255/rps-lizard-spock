722f0b42a017373590b54049d70d8ef040442e6b
Karol <karol@macbook-pro-dik.home>
2019-09-15 18:39:16 +0200

    Mute button implemented

--- Assets/Scripts/MuteButton.cs
+++ Assets/Scripts/MuteButton.cs
@@ -1,14 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MuteButton : MonoBehaviour
 {
     public AudioListener myAudioListener;
-    public Sprite[] icon;
+    public Sprite[] newIcon;
+    public Image currentIcon;
+
+    public void Start()
+    {
+        myAudioListener = FindObjectOfType<AudioListener>();
+
+        if (myAudioListener.enabled == false)
+        {
+            currentIcon.sprite = newIcon[0];
+        }
+    }
 
     public void ToggleSound()
     {
-        //myAudioListener.enabled(true);
+        if (myAudioListener.enabled)
+        {
+            myAudioListener.enabled = false;
+            currentIcon.sprite = newIcon[0];
+        }
+        else
+        {
+            myAudioListener.enabled = true;
+            currentIcon.sprite = newIcon[1];
+        }
     }
 }
